52efabebfd09c236079ecf8e4b2f6ba435aa0d05
zhanghao <zhanghao98@aliyun.com>
2020-09-10 21:20:13 +0800

    add goods module add template module add fastdfs module

--- changgou-parent/changgou-eureka/src/main/java/com/changgou/EurekaApplication.java
+++ changgou-parent/changgou-eureka/src/main/java/com/changgou/EurekaApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @Author:zhangHao
+ * @CreateDateTime : 2020-09-03  09 03 22:27
+ * @Description:
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(EurekaApplication.class,args);
     }
 }
